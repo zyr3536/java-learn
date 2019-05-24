5c82987eb01660ff3d57b7e85a4b5b0a0addf27d
zyr3536 <zyr3536@163.com>
2019-05-25 03:34:38 +0800

    vavr either

--- utils/src/main/java/org/util/vavr/EitherTest.java
+++ utils/src/main/java/org/util/vavr/EitherTest.java
@@ -1,14 +1,32 @@
 package org.util.vavr;
 
-import io.vavr.control.Either;
 
+import io.vavr.control.Either;
+import lombok.val;
+
+/**
+ * Either默认使用的是右值
+ */
 public class EitherTest {
     public static void main(String[] args) {
         EitherTest client = new EitherTest();
-        client.test1();
+//        client.test1();
+        client.test2();
     }
 
     public void test1() {
+        val a = Either.right(1).map(i -> i + 2).getOrElseThrow(() -> new RuntimeException("error"));
+        System.out.println(a);
+        val c = Either.right(1).filterOrElse(i -> i < 0, i -> 111).getLeft();
+        System.out.println(c);
+        val b = Either.right(1).swap().get();
+        System.out.println(b);
+    }
 
+    public void test2() {
+        int mark = 85;
+        Either either = mark < 85 ? Either.left("mark not acceptable") : Either.right(mark);
+        System.out.println(either);
+        // then one can do next steps, filter,map,and so on...
     }
 }
