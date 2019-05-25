7dcd463e33a0655594467fc69a5059ef82617d78
zyr3536 <zyr3536@163.com>
2019-05-25 15:23:12 +0800

    resilienct4j timelimiter

--- utils/src/main/java/org/resilience4j/timelimiter/Client.java
+++ utils/src/main/java/org/resilience4j/timelimiter/Client.java
@@ -2,8 +2,12 @@ package org.resilience4j.timelimiter;
 
 import io.github.resilience4j.timelimiter.TimeLimiter;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.vavr.control.Try;
 
 import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 /**
  * @author: zyr
@@ -11,13 +15,27 @@ import java.time.Duration;
  * @description: 时间控制
  */
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         TimeLimiterConfig config = TimeLimiterConfig.custom()
-                .timeoutDuration(Duration.ofSeconds(10))
+                .timeoutDuration(Duration.ofSeconds(1))
                 .cancelRunningFuture(true)
                 .build();
 
         TimeLimiter timeLimiter = TimeLimiter.of(config);
+
+        Supplier<CompletableFuture> futureSupplier = () -> CompletableFuture.supplyAsync(() -> {
+            long begin = System.currentTimeMillis();
+            while (begin + 10000 > System.currentTimeMillis()) {
+                // ...
+            }
+            return 1;
+        });
+
+
+        Callable<Integer> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, futureSupplier);
+
+        Try.of(callable :: call).onFailure(e -> System.out.println(e));
+
 
 
     }
