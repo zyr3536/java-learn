c41859558bbdfdf56f0ac82302cee97f7be48f04
yurenzhang <yurenzhang@ctrip.com>
2019-06-21 15:59:11 +0800

    spring boot init

---
+++ spring-learn/src/main/java/org/marco/springlearn/SpringLearnApplication.java
@@ -0,0 +1,13 @@
+package org.marco.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+
+}
